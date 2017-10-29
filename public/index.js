b3430642e35a6a8e5780e3174000957df66b5656
Gabi <gabriel.fresco@yahoo.com.ar>
2017-10-29 17:32:03 -0300

    Agrego controllers

--- public/index.js
+++ public/index.js
@@ -5,7 +5,11 @@ import notification from 'angular-ui-notification';
 import localyticsDirectives from 'angular-chosen-localytics';
 import routes from '../routes/routes.js';
 import deporteCtrl from '../js/admin/controllers/deporteController.js'
-import deporteService from '../js/admin/services/deporteService.js'
+import deporteService from '../js/services/deporteService.js'
+import torneoCtrl from '../js/admin/controllers/torneoController.js'
+import torneoService from '../js/services/torneoService.js'
+import premioCtrl from '../js/admin/controllers/premioController.js'
+import premioService from '../js/services/premioService.js'
 
 window.BASE_URL = "http://localhost:3001";
 
@@ -15,6 +19,10 @@ routes(app);
 
 deporteCtrl(app);
 deporteService(app);
+torneoCtrl(app);
+torneoService(app);
+premioCtrl(app);
+premioService(app);
 
 app.config(function ($urlRouterProvider, $locationProvider) {
   $urlRouterProvider.otherwise('/home');
